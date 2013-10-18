397440d80992184e6d48c3077e3fd31e0180cb7a
Jack Bradmiller-Feld <jbradmil@cern.ch>
2013-10-18 16:15:13 -0700

    Added function to make cutflow table for SMS samples.

--- src/make_cutflow_table.cpp
+++ src/make_cutflow_table.cpp
@@ -10,23 +10,30 @@
 #include "TFile.h"
 #include "TChain.h"
 
-
 typedef unsigned int uint;
 using namespace std;
 
-vector< pair <Cutflow*,string> > categories;
+vector< pair <Cutflow*,string> > categories, categories_sum, categories_susy;
+ofstream ofile;
 
-void MakeDefaultTable() { // Creates an AN-ready table of all the SM backgrounds and the data
+void MakeCompositionTable() { // Creates an AN-ready table of all the SM backgrounds
 
-  ofstream ofile;
-  ofile.open ("default_table.tex"); 
+  // aliases
+  ofile << "%Aliases" << endl ;
+  ofile << "\\newcommand{\\pt}{{p_T}}" << endl;
+  ofile << "\\newcommand{\\mdp}{\\Delta\\phi_{\\mathrm{min}}}" << endl;
+  ofile << "\\newcommand{\\mdR}{\\Delta R_{\\mathrm{max}}}" << endl;
+  ofile << "\\newcommand{\\metsig}{\\mathcal{S}}" << endl;
+  ofile << "\\newcommand{\\MET}{E_T^{\\mathrm{miss}}}" << endl;
+  ofile << endl << endl;
+  
   // Table header
   ofile << "\\begin{table}[h]" << endl;
   ofile << "\\centering" << endl;
-  ofile << "\\caption{Complete cutflow for SM MC and data. "
-        << "Preselection includes primary vertex, jet-$\\pt$, $\\MET-$cleaning, and $\\metsig>30$.}"<< endl;
-  ofile << "\\label{tab:full_cutflow}" << endl;
-  ofile << "\\begin{tabular}{cccccccc}" << endl;
+  ofile << "\\caption{Cutflow for SM MC backgrounds. "
+        << "Preselection includes primary vertex, jet-$\\pt$, $\\MET-$cleaning, and $\\metsig>30$ (see section \\ref{sec:basiccuts}).}"<< endl;
+  ofile << "\\label{tab:BG_cutflow}" << endl;
+  ofile << "\\begin{tabular}{cccccc}" << endl;
   ofile << "\\hline" << endl;
   ofile << "\\hline" << endl;
   for (uint cat(0);cat<categories.size();cat++) {
@@ -34,7 +41,7 @@ void MakeDefaultTable() { // Creates an AN-ready table of all the SM backgrounds
     ofile << categories[cat].second.c_str();
   }
   ofile << " \\\\ \\hline" << endl;
-        
+    
   uint nCuts = categories[0].first->numCutsTotal_;
   for (uint n(5);n<nCuts;n++) { // loop over cuts
     ofile << categories[0].first->cutNames_[n].c_str();
@@ -62,33 +69,127 @@ void MakeDefaultTable() { // Creates an AN-ready table of all the SM backgrounds
               << "$";
       }
     } // loop over samples
-    ofile << " \\\\" << endl;
+      ofile << " \\\\" << endl;
   } // loop over cuts
-        
-        
-  //}
-        
+    
   // Table footer
   ofile << "\\hline" << endl;
   ofile << "\\hline" << endl;
   ofile << "\\end{tabular}" << endl;
   ofile << "\\end{table}" << endl;
-        
-  // aliases
-  ofile << endl << endl << "%Aliases" << endl ;
-  ofile << "\\newcommand{\\pt}{{p_T}}" << endl;
-  ofile << "\\newcommand{\\mdp}{\\Delta\\phi_{\\mathrm{min}}}" << endl;
-  ofile << "\\newcommand{\\mdR}{\\Delta R_{\\mathrm{max}}}" << endl;
-  ofile << "\\newcommand{\\metsig}{\\mathcal{S}}" << endl;
-  ofile << "\\newcommand{\\MET}{E_T^{\\mathrm{miss}}}" << endl;
-        
+
+}
+
+void MakeSumTable() { // Creates an AN-ready table comparing the total SM background and data
+  ofile << endl << endl;
+  
+  // Table header
+  ofile << "\\begin{table}[h]" << endl;
+  ofile << "\\centering" << endl;
+  ofile << "\\caption{Cutflow comparison for SM MC and data. "
+        << "Preselection includes primary vertex, jet-$\\pt$, $\\MET-$cleaning, and $\\metsig>30$ (see section \\ref{sec:basiccuts}).}"<< endl;
+  ofile << "\\label{tab:BG_cutflow}" << endl;
+  ofile << "\\begin{tabular}{ccc}" << endl;
+  ofile << "\\hline" << endl;
+  ofile << "\\hline" << endl;
+  for (uint cat(0);cat<categories_sum.size();cat++) {
+    ofile << " & ";
+    ofile << categories_sum[cat].second.c_str();
+  }
+  ofile << " \\\\ \\hline" << endl;
+    
+  uint nCuts = categories_sum[0].first->numCutsTotal_;
+  for (uint n(5);n<nCuts;n++) { // loop over cuts
+    ofile << categories_sum[0].first->cutNames_[n].c_str();
+    for (uint cat(0);cat<categories_sum.size();cat++) { // loop over samples
+      int oomv = (int)floor(log10(categories_sum[cat].first->scaled_[n]));
+      int oome = (int)floor(log10(categories_sum[cat].first->error_[n]));
+      //printf("%f/%d/%f/%d\n",categories_sum[cat].first->scaled_[n],oomv,categories_sum[cat].first->error_[n],oome);
+      if (categories_sum[cat].second != "Data") { // MC formatting
+        if (oomv>2) {
+          ofile << "& $" << std::setprecision(oomv+1) << categories_sum[cat].first->scaled_[n] 
+                << "\\pm" << std::setprecision(oome+1) << categories_sum[cat].first->error_[n]
+                << "$";
+        }
+        else if (oomv>=-1&&oomv<=2) {
+          ofile << "& $" << std::setprecision(oomv+2) << categories_sum[cat].first->scaled_[n] 
+                << "\\pm" << std::setprecision(oome+2) << categories_sum[cat].first->error_[n]
+                << "$";
+        }
+        else {
+          ofile << "& $<0.1$";
+        }
+      }
+      else { // Data only
+        ofile << "& $" << std::setprecision(oomv+1) << categories_sum[cat].first->scaled_[n] 
+              << "$";
+      }
+    } // loop over samples
+    ofile << " \\\\" << endl;
+  } // loop over cuts
+    
+  // Table footer
+  ofile << "\\hline" << endl;
+  ofile << "\\hline" << endl;
+  ofile << "\\end{tabular}" << endl;
+  ofile << "\\end{table}" << endl;
+
+}
+
+void MakeSUSYTable() { // Creates an AN-ready table of select signal mass points
+  ofile << endl << endl;
+  // Table header
+  ofile << "\\begin{table}[h]" << endl;
+  ofile << "\\centering" << endl;
+  ofile << "\\caption{Cutflow for select SMS-TChiHH\\_2b2b\\_2J mass points. "
+        << "Preselection includes primary vertex, jet-$\\pt$, $\\MET-$cleaning, and $\\metsig>30$.}"<< endl;
+  ofile << "\\label{tab:susy_cutflow}" << endl;
+  ofile << "\\begin{tabular}{cccc}" << endl;
+  ofile << "\\hline" << endl;
+  ofile << "\\hline" << endl;
+  for (uint cat(0);cat<categories_susy.size();cat++) {
+    ofile << " & ";
+    ofile << categories_susy[cat].second.c_str();
+  }
+  ofile << " \\\\ \\hline" << endl;
+    
+  uint nCuts = categories_susy[0].first->numCutsTotal_;
+  for (uint n(5);n<nCuts;n++) { // loop over cuts
+    ofile << categories_susy[0].first->cutNames_[n].c_str();
+    for (uint cat(0);cat<categories_susy.size();cat++) { // loop over samples
+      int oomv = (int)floor(log10(categories_susy[cat].first->scaled_[n]));
+      int oome = (int)floor(log10(categories_susy[cat].first->error_[n]));
+      //printf("%f/%d/%f/%d\n",categories_susy[cat].first->scaled_[n],oomv,categories_susy[cat].first->error_[n],oome);
+      if (categories_susy[cat].second != "Data") { // MC formatting
+        if (oomv>2) {
+          ofile << "& $" << std::setprecision(oomv+1) << categories_susy[cat].first->scaled_[n] 
+                << "\\pm" << std::setprecision(oome+1) << categories_susy[cat].first->error_[n]
+                << "$";
+        }
+        else if (oomv>=-1&&oomv<=2) {
+          ofile << "& $" << std::setprecision(oomv+2) << categories_susy[cat].first->scaled_[n] 
+                << "\\pm" << std::setprecision(oome+2) << categories_susy[cat].first->error_[n]
+                << "$";
+        }
+        else {
+          ofile << "& $<0.1$";
+        }
+      }
+      else { // Data only
+        ofile << "& $" << std::setprecision(oomv+1) << categories_susy[cat].first->scaled_[n] 
+              << "$";
+      }
+    } // loop over samples
+    ofile << " \\\\" << endl;
+  } // loop over cuts
 
 }
 
 int main(int argc, char* argv[]) {
-        
+  
   // Option 1: print the cutflow from vectors of root files
   vector<TFile*> ttbar, qcd, wt, other, znn, SM, data;
+  vector<TFile*> m200, m300, m400;
 
   ttbar.push_back(new TFile("raw_plots_and_values/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7C-v2_AODSIM_UCSB1883_v71_SyncSkim.root","read"));
   ttbar.push_back(new TFile("raw_plots_and_values/TTJets_HadronicMGDecays_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A_ext-v1_AODSIM_UCSB1880_v71_SyncSkim.root","read"));
@@ -147,6 +248,10 @@ int main(int argc, char* argv[]) {
   data.push_back(new TFile("raw_plots_and_values/MET_Run2012D-PromptReco-v1_AOD_UCSB1870_v71_SyncSkim.root","read"));    
   // buggy! other.push_back(new TFile("raw_plots_and_values/ZH","read"));       
     
+  m200.push_back(new TFile("raw_plots_and_values/SMS-TChiHH_2b2b_2J_mChargino-200_mLSP-1_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V19_FSIM-v1_AODSIM_UCSB1872_v71_SyncSkim.root","read"));
+  m300.push_back(new TFile("raw_plots_and_values/SMS-TChiHH_2b2b_2J_mChargino-300_mLSP-1_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V19_FSIM-v1_AODSIM_UCSB1872_v71_SyncSkim.root","read"));
+  m400.push_back(new TFile("raw_plots_and_values/SMS-TChiHH_2b2b_2J_mChargino-400_mLSP-1_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V19_FSIM-v1_AODSIM_UCSB1871_v71_SyncSkim.root","read"));
+    
   Cutflow* ttbar_cutflow = new Cutflow(ttbar);
   //ttbar_cutflow->PrintCSV();
   Cutflow* qcd_cutflow = new Cutflow(qcd);
@@ -161,6 +266,13 @@ int main(int argc, char* argv[]) {
   //SM_cutflow->PrintCSV();
   Cutflow* data_cutflow = new Cutflow(data);
   //data_cutflow->PrintCSV();
+    
+  Cutflow* m200_cutflow = new Cutflow(m200);
+  //m200_cutflow->PrintCSV();
+  Cutflow* m300_cutflow = new Cutflow(m300);
+  //m300_cutflow->PrintCSV();
+  Cutflow* m400_cutflow = new Cutflow(m400);
+  //m400_cutflow->PrintCSV();
     
   // Option 2: use a list of files specified by the 'i' flag
   char in_filename[1024]="";
@@ -186,16 +298,25 @@ int main(int argc, char* argv[]) {
     Cutflow* in_cutflow = new Cutflow(in_filename,fileList);
     in_cutflow->Print();  
   }
-        
+  
   // Option 3: Make the default table
   categories.push_back(make_pair(ttbar_cutflow,"$t\\bar{t}$"));
   categories.push_back(make_pair(qcd_cutflow,"QCD"));
   categories.push_back(make_pair(wt_cutflow,"$W$, single top"));
   categories.push_back(make_pair(znn_cutflow,"$Z\\rightarrow$inv."));
   categories.push_back(make_pair(other_cutflow,"Other"));
-  categories.push_back(make_pair(SM_cutflow,"SM total"));
-  categories.push_back(make_pair(data_cutflow,"Data"));
-  MakeDefaultTable();
-    
+  
+  categories_sum.push_back(make_pair(SM_cutflow,"SM total"));
+  categories_sum.push_back(make_pair(data_cutflow,"Data"));
+  
+  categories_susy.push_back(make_pair(m200_cutflow,"$m_{\\chi}=200$ GeV"));
+  categories_susy.push_back(make_pair(m300_cutflow,"$m_{\\chi}=300$ GeV"));
+  categories_susy.push_back(make_pair(m400_cutflow,"$m_{\\chi}=400$ GeV"));
+  
+  ofile.open ("default_tables.tex"); 
+  MakeCompositionTable();
+  MakeSumTable();
+  MakeSUSYTable();
+  
   return 0;
 }
