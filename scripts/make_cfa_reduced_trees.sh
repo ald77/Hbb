b03f51d00b1b25e6d7445bc547c8660570d491c4
Adam Dishaw <aldishaw@gmail.com>
2014-01-30 15:21:42 -0800

    Add new ttbar files and remove old qcd samples

--- scripts/make_cfa_reduced_trees.sh
+++ scripts/make_cfa_reduced_trees.sh
@@ -7,6 +7,8 @@
 ./scripts/make_reduced_tree.exe -c -i TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7C-v1_AODSIM_UCSB1884_v71 &> logs/make_reduced_tree_1884.log &
 ./scripts/make_reduced_tree.exe -c -i TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V19_ext2-v1_AODSIM_UCSB1959_v71 &> logs/make_reduced_tree_1959.log &
 ./scripts/make_reduced_tree.exe -c -i TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V19_ext1-v1_AODSIM_UCSB1962_v71 &> logs/make_reduced_tree_1962.log &
+./scripts/make_reduced_tree.exe -c -i TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1850_v71 &> logs/make_reduced_tree_1962.log &
+./scripts/make_reduced_tree.exe -c -i TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V7C_FSIM-v2_AODSIM_UCSB1976_v71 &> logs/make_reduced_tree_1962.log &
 
 ./scripts/make_reduced_tree.exe -c -i TTWJets_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1857_v71 &> logs/make_reduced_tree_1857.log &
 ./scripts/make_reduced_tree.exe -c -i TTZJets_8TeV-madgraph_v2_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1856_v71 &> logs/make_reduced_tree_1856.log &
@@ -26,15 +28,15 @@
 
 ./scripts/make_reduced_tree.exe -c -i TTH_HToBB_M-125_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1855_v71 &> logs/make_reduced_tree_1855.log &
 
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM_UCSB1897_v71 &> logs/make_reduced_tree_1897.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1898_v71 &> logs/make_reduced_tree_1898.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-300to470_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1899_v71 &> logs/make_reduced_tree_1899.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-470to600_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1900_v71 &> logs/make_reduced_tree_1900.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-600to800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1901_v71 &> logs/make_reduced_tree_1901.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-800to1000_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1902_v71 &> logs/make_reduced_tree_1902.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-1000to1400_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1903_v71 &> logs/make_reduced_tree_1903.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-1400to1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1904_v71 &> logs/make_reduced_tree_1904.log &
-./scripts/make_reduced_tree.exe -c -i QCD_Pt-1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1905_v71 &> logs/make_reduced_tree_1905.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM_UCSB1897_v71 &> logs/make_reduced_tree_1897.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1898_v71 &> logs/make_reduced_tree_1898.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-300to470_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1899_v71 &> logs/make_reduced_tree_1899.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-470to600_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1900_v71 &> logs/make_reduced_tree_1900.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-600to800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1901_v71 &> logs/make_reduced_tree_1901.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-800to1000_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_UCSB1902_v71 &> logs/make_reduced_tree_1902.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-1000to1400_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1903_v71 &> logs/make_reduced_tree_1903.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-1400to1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1904_v71 &> logs/make_reduced_tree_1904.log &
+#./scripts/make_reduced_tree.exe -c -i QCD_Pt-1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1905_v71 &> logs/make_reduced_tree_1905.log &
 
 ./scripts/make_reduced_tree.exe -c -i BJets_HT-250To500_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1893_v71 &> logs/make_reduced_tree_1893.log &
 ./scripts/make_reduced_tree.exe -c -i BJets_HT-500To1000_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB1894_v71 &> logs/make_reduced_tree_1894.log &
@@ -57,9 +59,6 @@
 ./scripts/make_reduced_tree.exe -c -i MET_Run2012C-PromptReco-v2_AOD_UCSB1867_v71 &> logs/make_reduced_tree_1867.log &
 ./scripts/make_reduced_tree.exe -c -i MET_Run2012D-PromptReco-v1_AOD_UCSB1869_v71 &> logs/make_reduced_tree_1869.log &
 ./scripts/make_reduced_tree.exe -c -i MET_Run2012D-PromptReco-v1_AOD_UCSB1870_v71 &> logs/make_reduced_tree_1870.log &
-
-./scripts/make_reduced_tree.exe -c -i SMS-TChiHH_2b2b_2J_mChargino-130to325_mLSP-1to195_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V19_FSIM-v1_AODSIM_UCSB1872_v71 &> logs/make_reduced_tree_1872.log &
-./scripts/make_reduced_tree.exe -c -i SMS-TChiHH_2b2b_2J_mChargino-350to500_mLSP-1to370_TuneZ2star_8TeV-madgraph-tauola_Summer12-START53_V19_FSIM-v1_AODSIM_UCSB1871_v71 &> logs/make_reduced_tree_1871.log &
 
 wait
 
