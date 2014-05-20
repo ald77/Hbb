a579d7f234efea1ed590ca80afef7f50d8bf071a
ald77 <aldishaw@gmail.com>
2014-05-20 12:23:01 +0200

    Allow for pointers in template functions (could probably be done better).

--- inc/math.hpp
+++ inc/math.hpp
@@ -43,8 +43,27 @@ namespace Math{
   }
 
   template<typename T>
+  T Sum(T* begin, T* end){
+    //Performs Kahan summation (more precise than naive summation for long lists of numbers)
+    T sum(0.0);
+    volatile T correction(0.0);
+    for(; begin!=end; ++begin){
+      const T corrected_val(*begin-correction);
+      const T temp_sum(sum+corrected_val);
+      correction=(temp_sum-sum)-corrected_val;
+      sum=temp_sum;
+    }
+    return sum;
+  }
+
+  template<typename T>
   typename T::value_type Mean(T begin, T end){
     return Sum(begin, end)/static_cast<typename T::value_type>(std::distance(begin,end));
+  }
+
+  template<typename T>
+  T Mean(T* begin, T* end){
+    return Sum(begin, end)/static_cast<T>(std::distance(begin,end));
   }
 
   template<typename T>
@@ -59,10 +78,36 @@ namespace Math{
   }
 
   template<typename T>
+  T Variance(T* begin, T* end){
+    const T mean(Mean(begin, end));
+    std::vector<T> residuals_squared(0);
+    for(T* it(begin); it!=end; ++it){
+      const T residual(*it-mean);
+      residuals_squared.push_back(residual*residual);
+    }
+    return Sum(residuals_squared.begin(), residuals_squared.end())/static_cast<T>(std::distance(begin,end));
+  }
+
+  template<typename T>
   typename T::value_type Median(T begin, T end){
     //N.B.: As implemented, this function may reorder elements in the input range.
     std::sort(begin, end);
     const typename T::difference_type separation(std::distance(begin, end));
+    end=begin;
+    std::advance(end, floor(0.5*separation));
+    if(separation & 0x1ul){
+      return *end;
+    }else{
+      std::advance(begin, floor(0.5*separation)-1);
+      return 0.5*((*begin)+(*end));
+    }
+  }
+
+  template<typename T>
+  T Median(T* begin, T* end){
+    //N.B.: As implemented, this function may reorder elements in the input range.
+    std::sort(begin, end);
+    const unsigned long separation(std::distance(begin, end));
     end=begin;
     std::advance(end, floor(0.5*separation));
     if(separation & 0x1ul){
@@ -89,6 +134,35 @@ namespace Math{
       ++inner_end;
       while(inner_end!=end){
         const typename T::value_type this_delta((*inner_end)-(*inner_begin));
+        if(this_delta<min_delta){
+          min_delta=this_delta;
+          min_begin=inner_begin;
+          min_end=inner_end;
+        }
+        ++inner_begin;
+        ++inner_end;
+      }
+      return HalfSampleMode(min_begin, min_end);
+    }
+  }
+
+
+  template<typename T>
+  T HalfSampleMode(T* begin, T* end){
+    //N.B.: As implemented, this function may reorder elements in the input range.
+    std::sort(begin, end);
+    const unsigned long separation(std::distance(begin, end));
+    if(separation<=1){
+      return 0.5*((*begin)+(*end));
+    }else{
+      T* inner_begin(begin), inner_end(begin);
+      std::advance(inner_end, ceil(0.5*separation));
+      T min_delta((*inner_end)-(*inner_begin));
+      T* min_begin(inner_begin), min_end(inner_end);
+      ++inner_begin;
+      ++inner_end;
+      while(inner_end!=end){
+        const T this_delta((*inner_end)-(*inner_begin));
         if(this_delta<min_delta){
           min_delta=this_delta;
           min_begin=inner_begin;
